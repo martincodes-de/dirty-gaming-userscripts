6e483583bb36e6912fd9e65c0bfa320e1237a5e3
martincodes <62392843+martincodes-de@users.noreply.github.com>
2021-06-02 19:08:12 +0200

    update und download url hinzugefügt

--- userscript-status-leitstellensystem.user.js
+++ userscript-status-leitstellensystem.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Dirty-Gaming.com Leitstellensystemverbesserung: Status für Userscripts anzeigen
-// @version      1.0.0
+// @version      1.0.1
 // @description  Zeigt im Menü an, dass Userscripts verwendet werden.
 // @author       martincodes
 // @match        https://leitstelle.dirty-gaming.com/*
 // @match        file:///C:/Users/Martin/Desktop/Dirty-Leitstellensystem/*
 // @icon         https://i.imgur.com/q2zMMHS.png
-// @updateURL
-// @downloadURL
+// @updateURL    https://raw.githubusercontent.com/martincodes-de/dirty-gaming-userscripts/main/userscript-status-leitstellensystem.user.js
+// @downloadURL  https://raw.githubusercontent.com/martincodes-de/dirty-gaming-userscripts/main/userscript-status-leitstellensystem.user.js
 // @grant        none
 // ==/UserScript==
 
