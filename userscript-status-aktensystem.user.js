24a0e721a885b4900f6a2ff4bec68f0e663d8ffa
martincodes <62392843+martincodes-de@users.noreply.github.com>
2021-06-02 22:49:59 +0200

    typo

--- userscript-status-aktensystem.user.js
+++ userscript-status-aktensystem.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Dirty-Gaming.com Aktensystemverbesserung: Status für Userscripts anzeigen
-// @version      1.0.2
+// @version      1.0.3
 // @description  Zeigt im Menü an, dass Userscripts verwendet werden.
 // @author       martincodes
 // @match        https://akte.dirty-gaming.com/*
@@ -14,4 +14,4 @@
 
 var menuTitel = document.getElementById("navName");
 
-menuTitel.innerHTML = "<b>Atensystem</b><br><small>Userscripts aktiviert :-)</small>";
+menuTitel.innerHTML = "<b>Aktensystem</b><br><small>Userscripts aktiviert :-)</small>";
