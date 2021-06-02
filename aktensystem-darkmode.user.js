70d9c6702024e5338e548e7a99b26fd4b630c2bf
martincodes <62392843+martincodes-de@users.noreply.github.com>
2021-06-02 20:38:06 +0200

    darkmode v1
    - bußgeldrechner ausgetauscht
    - darkmode eingefügt

--- aktensystem-darkmode.user.js
+++ aktensystem-darkmode.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Dirty-Gaming.com Aktensystemverbesserung: Darkmode
-// @version      1.0.1
+// @version      1.0.2
 // @description  Nutzt einen Darkmode im Aktensystem
 // @author       martincodes
 // @match        https://akte.dirty-gaming.com/*
@@ -11,6 +11,37 @@
 // @grant        none
 // ==/UserScript==
 
-var headTag = document.getElementsByTagName("head")[0];
-var neuesStyleTag = "<link rel='stylesheet' href='https://raw.githubusercontent.com/martincodes-de/dirty-gaming-userscripts/main/aktensystem-darkmode.css'/>";
-headTag.innerHTML += neuesStyleTag;
+var styles = [
+  "body {background-color: #1c1a1a; color: white;}",
+    ".card {background-color: #262626; color: white !important;}",
+    "table {color: #fff !important;}",
+    ".form-control {color: black; !important}",
+    ".form-control:focus {color: black; !important}",
+    "h1, h2, h3, h4, h5, h6 {color: white; !important}",
+    ".verlaufaktentabelle {color: white !important}",
+    ".verlaufaktenname {color: white !important}",
+    ".bußgeldtag {border: 0}"
+];
+
+var styletag = "<style>";
+
+for (let i = 0; i < styles.length; i++) {
+    styletag += " "+styles[i];
+}
+
+styletag += "</style>";
+
+document.getElementsByTagName("head")[0].innerHTML += styletag;
+
+/* Bußgeldrechner ersetzen */
+
+var bussgeldrechnerDIV = "<div id='bussgeldrechner' class='bußgeldtag'>" +
+        "<div class='btn-group' role='group'>" +
+            "<a href='https://bussgeldrechner.lspd-dirty.de' target='_blank' class='btn btn-primary btn-sm'>RTS Bußgeldrechner</a>" +
+            "<a href='https://docs.google.com/spreadsheets/d/1OjeNXUC13ciAv3YPX6NiGCHVLyjLrZx4Q1WkkejkmUc/edit#gid=424553797' target='_blank' class='btn btn-secondary btn-sm'>Bußgeldrechner 2.0</a>" +
+        "</div>" +
+    "</div>";
+
+document.getElementsByClassName("bußgeldtag")[0].outerHTML = bussgeldrechnerDIV; // Strafsacheakten Button
+document.getElementsByClassName("bußgeldtag")[1].outerHTML = bussgeldrechnerDIV; // Bußgeldakten Button
+
