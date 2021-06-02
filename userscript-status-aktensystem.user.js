3d566b77e3dccdf4510ea8f738a149f9c193a7bd
martincodes <62392843+martincodes-de@users.noreply.github.com>
2021-06-02 18:48:51 +0200

    lokale datei hinzugefügt

--- userscript-status-aktensystem.user.js
+++ userscript-status-aktensystem.user.js
@@ -4,6 +4,7 @@
 // @description  Zeigt im Menü an, dass Userscripts verwendet werden.
 // @author       martincodes
 // @match        https://akte.dirty-gaming.com/*
+// @match        file:///C:/Users/Martin/Desktop/Dirty-Aktensystem/*
 // @icon         https://i.imgur.com/q2zMMHS.png
 // @updateURL    https://raw.githubusercontent.com/martincodes-de/dirty-gaming-userscripts/main/userscript-status-aktensystem.user.js
 // @downloadURL  https://raw.githubusercontent.com/martincodes-de/dirty-gaming-userscripts/main/userscript-status-aktensystem.user.js
