cba34e010ff32378925aa413121ea5cdc16e7daf
martincodes <62392843+martincodes-de@users.noreply.github.com>
2021-07-04 15:29:16 +0200

    lspdbsys hinzugefügt

--- aktensystem-bussgeldrechner-tausch.user.js
+++ aktensystem-bussgeldrechner-tausch.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Dirty-Gaming.com Aktensystemverbesserung: Bußgeldrechnertausch
-// @version      1.0.0
+// @version      1.0.1
 // @description  Tauscht den Bußgeldrechnerbutton mit den Bußgeldrechnern, die wir nutzen, da der interne Rechner nicht funktional ist.
 // @author       martincodes
 // @match        https://akte.dirty-gaming.com/buerger/*
@@ -16,6 +16,7 @@ var bussgeldrechnerDIV = "<div id='bussgeldrechner' class='bußgeldtag' style='b
     "<div class='btn-group' role='group'>" +
     "<a href='https://bussgeldrechner.lspd-dirty.de' target='_blank' class='btn btn-primary btn-sm'>RTS Bußgeldrechner</a>" +
     "<a href='https://docs.google.com/spreadsheets/d/1OjeNXUC13ciAv3YPX6NiGCHVLyjLrZx4Q1WkkejkmUc/edit#gid=424553797' target='_blank' class='btn btn-secondary btn-sm'>Bußgeldrechner 2.0</a>" +
+    "<a href='http://lspdbsys.lspd-dirty.de' target='_blank' class='btn btn-info btn-sm'>LSPDBSYS</a>" +
     "</div>" +
     "</div>";
 
