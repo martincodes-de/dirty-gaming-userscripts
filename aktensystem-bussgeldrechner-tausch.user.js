fafa4833dd65438ec04249d3c1a9d5f8dcd2b239
martincodes <62392843+martincodes-de@users.noreply.github.com>
2021-07-18 13:39:29 +0200

    Versucht wird nun alle Bußgeldtags anzusprechen + zu ersetzen
    + Bußgeldrechner 2.0 entfernt

--- aktensystem-bussgeldrechner-tausch.user.js
+++ aktensystem-bussgeldrechner-tausch.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Dirty-Gaming.com Aktensystemverbesserung: Bußgeldrechnertausch
-// @version      1.0.1
+// @version      1.0.2
 // @description  Tauscht den Bußgeldrechnerbutton mit den Bußgeldrechnern, die wir nutzen, da der interne Rechner nicht funktional ist.
 // @author       martincodes
 // @match        https://akte.dirty-gaming.com/buerger/*
@@ -12,13 +12,16 @@
 // @run-at       document-body
 // ==/UserScript==
 
+var bussgeldtags = document.getElementsByClassName("bußgeldtag");
+
 var bussgeldrechnerDIV = "<div id='bussgeldrechner' class='bußgeldtag' style='border:0'>" +
     "<div class='btn-group' role='group'>" +
     "<a href='https://bussgeldrechner.lspd-dirty.de' target='_blank' class='btn btn-primary btn-sm'>RTS Bußgeldrechner</a>" +
-    "<a href='https://docs.google.com/spreadsheets/d/1OjeNXUC13ciAv3YPX6NiGCHVLyjLrZx4Q1WkkejkmUc/edit#gid=424553797' target='_blank' class='btn btn-secondary btn-sm'>Bußgeldrechner 2.0</a>" +
     "<a href='http://lspdbsys.lspd-dirty.de' target='_blank' class='btn btn-info btn-sm'>LSPDBSYS</a>" +
     "</div>" +
     "</div>";
 
-document.getElementsByClassName("bußgeldtag")[0].outerHTML = bussgeldrechnerDIV; // Strafsacheakten Button
-document.getElementsByClassName("bußgeldtag")[1].outerHTML = bussgeldrechnerDIV; // Bußgeldakten Button
+for (let i = 0; i < bussgeldtags.length; i++) {
+    let bussgeldtag = bussgeldtags[i];
+    bussgeldtag.outerHTML = bussgeldrechnerDIV
+}
